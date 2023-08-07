3d013f4a5c24ac6e5ae0007ca75be5c128a2e31d
Fran Zekan <zekan.fran369@gmail.com>
2023-08-07 12:03:07 +0200

    Shorter expiration time

--- lib/rails-settings/base.rb
+++ lib/rails-settings/base.rb
@@ -127,7 +127,7 @@ module RailsSettings
       end
 
       def _all_settings
-        RequestCache.all_settings ||= Rails.cache.fetch(cache_key, expires_in: 1.week) do
+        RequestCache.all_settings ||= Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
           vars = unscoped.select("var, value")
           result = {}
           vars.each { |record| result[record.var] = record.value }
